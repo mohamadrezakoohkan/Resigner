b2185825dc52b0c90c3d212333044e99ff30b600
mohamad reza koohkan <mohamad_koohkan@icloud.com>
2020-04-05 02:04:09 +0300

    initial commit

---
+++ methods.sh
@@ -0,0 +1,55 @@
+
+checkSigningCertificates() {
+    security find-identity -v -p codesigning
+}
+
+step() {
+    stepper=$((stepper + 1))
+    echo ${cyan}${bold}[${stepper}]${norm} ${green}$1${norm};
+}
+
+unarchive() {
+    read file
+    unzip -q ${file}.ipa
+    cd Payload
+    cd */
+}
+
+archive() {
+    read ipaName
+    zip -q -r ${ipaName}.ipa Payload
+}
+
+locate() {
+    pwd
+    pwd | pbcopy
+}
+
+delete() {
+    rm -rf $1
+}
+
+warning() {
+    echo "${yellow}${bold}Please answer y for yes or n for no.${norm}"
+}
+
+verificationRequest() {
+ step "$1 ${yellow}${bold}[y/n] ${norm}";
+}
+
+verify() {
+    while true; do
+        read yn
+        case $yn in
+            "y" ) delete $1; break;;
+            "n" ) break;;
+            * ) warning;;
+        esac
+    done
+}
+
+setProfile() {
+    cp ../../$1.mobileprovision embedded.mobileprovision
+    cd ../..
+}
+
